007f5d7b4d97228812b91ef4bd207c152e9166d1
Slavik <diod-triod@ukr.net>
2022-02-27 15:53:43 +0200

    stage2

--- OpticalReaderConfigurator/TransportLayerUart.cs
+++ OpticalReaderConfigurator/TransportLayerUart.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports
 
 namespace OpticalReaderConfigurator
 {
     public class TransportLayerUart
     {
+        private bool _connect = false;
+        private SerialPort _serialPort;
+        public delegate void connDisconEvent();
+        public event connDisconEvent connectEv;
+        public event connDisconEvent disconnectEv;
+
         public TransportLayerUart()
         {
 
         }
-        private bool connect = false;
-
-        delegate void event();
-        event AccountHandler Notify;
-
+        
         public bool isConnect()
         {
-            return connect;
+            return _connect;
         }
+
+        public void connect()
+        {
+
+        }
+
+        public void disconnect()
+        {
+
+        }
+
+        public void setSettings()
+        {
+
+        }
+
 
 
     }
